e252227d7503b51414bd727e63da8a8caed53a0f
iMom0 <mobeiheart@gmail.com>
2011-06-15 22:38:41 +0800

    Jiong

---
+++ filter.js
@@ -0,0 +1,36 @@
+/*
+ 
+BMY filter 0.9.0
+
+A javascript file to filter BMY BBS items.
+
+Created by iMom0 (Mobeiheart[at]GMail.COM)
+Fri Feb 25,2011
+
+*/
+
+function filter()
+{
+    function reInit() {
+        chrome.extension.sendRequest({req_ids:"now"},function(response){ localStorage["bmy"] = response.ids;});
+        var list = localStorage["bmy"];
+        var re = new RegExp(list,"i");
+        return re;
+    }
+
+    list_re = reInit();
+
+    function displayNone(obj) {
+        obj.style.display = "none";
+    }
+
+    var base = document.getElementsByTagName("table");
+
+    for (var i=7;i<base.length;i++)
+    {
+        var source_html = base[i].getElementsByTagName("a")[3].innerHTML;
+        if (list_re.test(source_html)) displayNone(base[i]);
+    }
+}
+
+filter();
