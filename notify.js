8ea59f2e56bf04ada4b9027fa2cc4c3e114b6146
imom0 <mobeiheart@gmail.com>
2013-10-16 10:02:38 +0800

    给notify.js加上了大体的功能

--- notify.js
+++ notify.js
@@ -1,9 +1,20 @@
 $(function() {
   var fetchNotification = function() {
     var path = document.location.pathname.split('/')[1],
-        url = document.location.protocol + "//" + document.location.hostname + "/" + path + '/notify',
+        url = document.location.protocol + "//" + document.location.hostname + "/" + path + '/bbsnotify',
         page = $.parseHTML($.getJSON(url)),
-        unreadCount = $('.notify-item', page).length;
+        unreadCount = $('.notify-item', page).length,
+        unreadCountKey = 'bmy-unread-count',
+        cachedUnreadCount = localStorage.getItem(unreadCountKey) || 0;;
+        if (window.webkitNotifications.checkPermission() == 0) { 
+          if (unreadCount > cachedUnreadCount) {
+              window.webkitNotifications.createNotification('img/notify.png', '通知', '有新的BMY提醒...').show();
+          }
+        } else {
+          window.webkitNotifications.requestPermission();
+        }
+        localStorage.setItem(unreadCountKey, unreadCount);
   }
+  window.setInterval(fetchNotification, 60 * 1000);
   fetchNotification();
 })
