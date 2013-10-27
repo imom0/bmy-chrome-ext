7cffb0aa3578650e3bf622181dc7dc66aa511e97
imom0 <mobeiheart@gmail.com>
2013-10-27 21:11:04 +0800

    Use jQuery.get

--- notify.js
+++ notify.js
@@ -11,25 +11,23 @@ $(function() {
   fetchNotification = function() {
     var bmyUrl = getBmyUrl();
     if (!!bmyUrl) {
-      var ident = bmyUrl.split('/')[3],
-          url = "http://bbs.xjtu.edu.cn/" + ident + '/bbsnotify',
-          xhr = new XMLHttpRequest();
-          xhr.onload = function() {
-            page = $.parseHTML(xhr.responseText),
-            unreadCount = $('.notify-item a[href^=bbscon]', page).length,
-            unreadCountKey = 'bmy-unread-count',
-            cachedUnreadCount = localStorage.getItem(unreadCountKey) || 0;
-            if (unreadCount > cachedUnreadCount) {
-              var notification = window.webkitNotifications.createNotification('img/notify.png', '通知', '有新的BMY提醒...');
-              notification.onclick = function() {
-                chrome.tabs.create({url: url});
-              }
-              notification.show();
-            }
-            localStorage.setItem(unreadCountKey, unreadCount);
+      var page,
+          ident = bmyUrl.split('/')[3],
+          url = "http://bbs.xjtu.edu.cn/" + ident + '/bbsnotify';
+      $.get(url, function(data) {
+        page = $.parseHTML(data),
+        unreadCount = $('.notify-item a[href^=bbscon]', page).length,
+        unreadCountKey = 'bmy-unread-count',
+        cachedUnreadCount = localStorage.getItem(unreadCountKey) || 0;
+        if (unreadCount > cachedUnreadCount) {
+          var notification = window.webkitNotifications.createNotification('img/notify.png', '通知', '有新的BMY提醒...');
+          notification.onclick = function() {
+            chrome.tabs.create({url: url});
           }
-          xhr.open('GET', url);
-          xhr.send();
+          notification.show();
+        }
+        localStorage.setItem(unreadCountKey, unreadCount);
+      });
     }
   }
   window.setInterval(fetchNotification, 30 * 1000);
